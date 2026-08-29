46b36ecf5add479a6df308507640f12e43a23681
Eliaszn <farellh12@gmail.com>
2026-08-29 09:13:11 +0700

    feat: add stock data API handler using yahoo-finance2 to fetch financial metrics

--- api/stock.mjs
+++ api/stock.mjs
@@ -1,4 +1,5 @@
-import yahooFinance from "yahoo-finance2";
+import YahooFinance from "yahoo-finance2";
+const yahooFinance = new YahooFinance();
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
